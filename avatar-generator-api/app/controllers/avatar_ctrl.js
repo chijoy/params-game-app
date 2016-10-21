9dc43f3ad85b3ed7a2421e471ff6de3612099d0a
Joy <joymccaffreychicago@gmail.com>
2016-10-20 21:11:34 -0500

    Remove duplicate sign up sign in and login and search now that it's on the top nav bar.

--- avatar-generator-api/app/controllers/avatar_ctrl.js
+++ avatar-generator-api/app/controllers/avatar_ctrl.js
@@ -3,7 +3,7 @@
 
   angular.module("app").controller("avatarCtrl", function($scope, $http) {
     $scope.setup = function() {
-      $http.get("/api/v1/avatars.json").then(function(response){
+      $http.get("/api/v1/avatars.json").then(function(response) {
         $scope.avatar = response.data;
       });                
     };
