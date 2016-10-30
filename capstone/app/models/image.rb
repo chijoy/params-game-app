ce357dac355daf74afafb176f0ede32b661e09b9
Joy <joymccaffreychicago@gmail.com>
2016-10-30 15:32:22 -0500

    Add strftime.

--- capstone/app/models/image.rb
+++ capstone/app/models/image.rb
@@ -2,4 +2,12 @@ class Image < ApplicationRecord
   belongs_to :user
   has_many :image_gangs
   has_many :gangs, through: :image_gangs
+
+  def pretty_created_at
+    created_at.strftime("%m/%d/%Y")
+  end
+
+  def pretty_updated_at
+    updated_at.strftime("%m/%d/%Y")
+  end
 end
