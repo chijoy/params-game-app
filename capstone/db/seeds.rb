a293dddea6c4574c690c4cb538421832c716fbb8
Joy <joymccaffreychicago@gmail.com>
2016-11-11 17:22:55 -0600

    Add one line of seed data.

--- capstone/db/seeds.rb
+++ capstone/db/seeds.rb
@@ -1,9 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
+# Image.create([
+#   {status:"Open", type_of_surface: "Metal - Painted", surface_location: "Front", assign_to_gang: "Latin Kings", latitude: 41.95954735283962, longitude: -87.78173752256184}
+# ])
 
+Image.create([
+  {status:"Open", type_of_surface: "Other/Unknown Surface", surface_location: "Alley", assign_to_gang: "Maniac Latin Disciples", latitude: 41.943705665469096, longitude: -87.74518896580682, user_id: 6,}
+])
