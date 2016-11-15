f7e0d6f7bfe0842230787711f7ece2ec6b476d6c
Joy <joymccaffreychicago@gmail.com>
2016-11-14 20:34:09 -0600

    Revert "Get the search working again for gang and status."
    This reverts commit 392b23ae91ada87f3617c1f3362f34c997b02897.

--- capstone/db/seeds.rb
+++ capstone/db/seeds.rb
@@ -1,5 +1,9 @@
 
 Image.create([
-  {status:"Completed", type_of_surface: "Brick - Unpainted", surface_location: "Front", assign_to_gang: "Spanish Lords", latitude: 41.92212036742885, longitude: -87.7599961527632, user_id: 6}
- 
+  {status:"Open", type_of_surface: "Metal - Painted", surface_location: "Dumpster", assign_to_gang: "Almighty Simon City Royals", latitude: 41.9521105388302, longitude: -87.76822492256562, user_id: 6},
+  {status:"Open", type_of_surface: "Metal - Unainted", surface_location: "Fence", assign_to_gang: "Conservative Vice Lords", latitude: 41.99098192566919, longitude: -87.69683456245063, user_id: 6},
+  {status:"Open", type_of_surface: "Brick - Unainted", surface_location: "Front", assign_to_gang: "Almighty Simon City Royals", latitude: 41.96028969049714, longitude: -87.77356887924822, user_id: 6},
+  {status:"Open", type_of_surface: "Brick - Unainted", surface_location: "Side", assign_to_gang: "Adidas Boys", latitude: 41.95477731086982, longitude: -87.66233745396293, user_id: 6},
+  {status:"Open", type_of_surface: "Limestone", surface_location: "Front", assign_to_gang: "Latin Kings", latitude: 41.976191717469526, longitude: -87.67597432453286, user_id: 6},
+  {status:"Open", type_of_surface: "Metal - Painted", surface_location: "Mail Box", assign_to_gang: "Latin Kings", latitude: 41.966806963348446, longitude: -87.6792916510176, user_id: 6}
 ])
