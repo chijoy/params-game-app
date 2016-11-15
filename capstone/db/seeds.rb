366de8c7b29af3acedc1eee0e8e02dea3f10aacf
Joy <joymccaffreychicago@gmail.com>
2016-11-15 12:52:34 -0600

    Seed data and format submit buttons.

--- capstone/db/seeds.rb
+++ capstone/db/seeds.rb
@@ -1,9 +1,5 @@
 
 Image.create([
-  {status:"Open", type_of_surface: "Metal - Painted", surface_location: "Dumpster", assign_to_gang: "Almighty Simon City Royals", latitude: 41.9521105388302, longitude: -87.76822492256562, user_id: 6},
-  {status:"Open", type_of_surface: "Metal - Unainted", surface_location: "Fence", assign_to_gang: "Conservative Vice Lords", latitude: 41.99098192566919, longitude: -87.69683456245063, user_id: 6},
-  {status:"Open", type_of_surface: "Brick - Unainted", surface_location: "Front", assign_to_gang: "Almighty Simon City Royals", latitude: 41.96028969049714, longitude: -87.77356887924822, user_id: 6},
-  {status:"Open", type_of_surface: "Brick - Unainted", surface_location: "Side", assign_to_gang: "Adidas Boys", latitude: 41.95477731086982, longitude: -87.66233745396293, user_id: 6},
-  {status:"Open", type_of_surface: "Limestone", surface_location: "Front", assign_to_gang: "Latin Kings", latitude: 41.976191717469526, longitude: -87.67597432453286, user_id: 6},
-  {status:"Open", type_of_surface: "Metal - Painted", surface_location: "Mail Box", assign_to_gang: "Latin Kings", latitude: 41.966806963348446, longitude: -87.6792916510176, user_id: 6}
+  {status:"Open", type_of_surface: "Other/Unknown Surface", surface_location: "Side", assign_to_gang: "Conservative Vice Lords", latitude: 41.91557036644772, longitude: -87.65099514640393, user_id: 6},
+
 ])
