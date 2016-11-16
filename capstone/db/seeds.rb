0b96ac73c79447d62b2c939ec7b0ff8ebb81d80d
Joy <joymccaffreychicago@gmail.com>
2016-11-15 18:03:07 -0600

    Remove extraneous fields from edit.

--- capstone/db/seeds.rb
+++ capstone/db/seeds.rb
@@ -1,5 +1,16 @@
 
 Image.create([
-  {status:"Open", type_of_surface: "Other/Unknown Surface", surface_location: "Side", assign_to_gang: "Conservative Vice Lords", latitude: 41.91557036644772, longitude: -87.65099514640393, user_id: 6},
-
+  {status:"Open", type_of_surface: "Other / Unknown Surface", surface_location: "Front", assign_to_gang: "Spanish Gangster Disciples", latitude: 41.974552780481666, longitude: -87.76835410211622, user_id: 6},
+  {status:"Open", type_of_surface: "Metal - Painted", surface_location: "Traffic Control Box", assign_to_gang: "Spanish Gangster Disciples", latitude: 41.96838329353663, longitude: -87.72329202562146, user_id: 6},
+  {status:"Completed", type_of_surface: "Aluminum Siding", surface_location: "Door", assign_to_gang: "Spanish Gangster Disciples", latitude: 41.96818385506882, longitude: -87.7232023362117, user_id: 6},
+  {status:"Completed", type_of_surface: "Aluminum Siding", surface_location: "Door", assign_to_gang: "Spanish Gangster Disciples", latitude: 41.9683991905832, longitude: -87.72355115796904, user_id: 6},
+  {status:"Completed", type_of_surface: "Metal - Painted", surface_location: "Fence", assign_to_gang: "Spanish Gangster Disciples", latitude: 41.97153984004936, longitude: -87.74596833146853, user_id: 6},
+  {status:"Completed", type_of_surface: "Metal - Painted", surface_location: "Fence", assign_to_gang: "Spanish Gangster Disciples", latitude: 41.96872925486523, longitude: -87.68754443241868, user_id: 6},
+  {status:"Completed", type_of_surface: "Metal - Painted", surface_location: "Fence", assign_to_gang: "Spanish Gangster Disciples", latitude: 41.961518956184065, longitude: -87.74276400861359, user_id: 6},
+  {status:"Completed", type_of_surface: "Metal - Painted", surface_location: "Front", assign_to_gang: "Spanish Gangster Disciples", latitude: 41.96817438867154, longitude: -87.72406340372288, user_id: 6},
+  {status:"Completed", type_of_surface: "Metal - Painted", surface_location: "Dumpster", assign_to_gang: "Spanish Gangster Disciples", latitude: 41.96453937284386, longitude: -87.74278719599451, user_id: 6},
+  {status:"Completed", type_of_surface: "Metal - Painted", surface_location: "Traffic Control Box", assign_to_gang: "Spanish Gangster Disciples", latitude: 41.96881761625658, longitude: -87.66305226324998, user_id: 6},
+  {status:"Completed", type_of_surface: "Metal - Painted", surface_location: "Traffic Control Box", assign_to_gang: "Spanish Gangster Disciples", latitude: 41.96867954024001, longitude: -87.66599895111777, user_id: 6},
+  {status:"Completed", type_of_surface: "Metal - Painted", surface_location: "Pole", assign_to_gang: "Spanish Gangster Disciples", latitude: 41.968690437018545, longitude: -87.67583799800329, user_id: 6},
+  {status:"Completed", type_of_surface: "Metal - Painted", surface_location: "Mail Box", assign_to_gang: "Spanish Gangster Disciples", latitude: 41.96660306169342, longitude: -87.71957038182508, user_id: 6}
 ])
